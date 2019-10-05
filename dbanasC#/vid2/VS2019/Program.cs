54de5c0b799572d498717c95b727009bea7b170f
Matthew Yates <Mat.C.Yates@gmail.com>
2019-10-05 16:24:37 -0400

    explored multidimensional arrays

--- dbanasC#/vid2/VS2019/Program.cs
+++ dbanasC#/vid2/VS2019/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Globalization;
 
 namespace VS2019
 {
@@ -7,7 +13,24 @@ namespace VS2019
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //use var for implicit typing
+            //var intval = 1;
+            // in c# all types have a base type of object
+            // this mean you could make an object[] array to store values of different types in a single array;
+            // multi dimensional arrays are created via the following syntax
+            // int[5,5]
+            //ex
+            //string[,] custNames = new string[2, 2] { {"bob", "smith" }, {"tom", "jerry" } };
+            //for (int i = 0; i < custNames.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < custNames.GetLength(1); j++)
+            //    {
+            //        Console.WriteLine($"{custNames[i, j]} [{i},{j}]");
+            //    }
+            //}
+
+
+            Console.ReadLine();
         }
     }
 }
